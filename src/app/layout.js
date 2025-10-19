4be804abb12f2d8ca6312ec658feaa34dd3b637c
Eljay <149206706+Van1a@users.noreply.github.com>
2025-10-19 21:38:59 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
-
+export const metadata = {
+  title: "Nerium API",
+  description: "Very reliable when it comes to Game Code — supported more than 300+ Games!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
