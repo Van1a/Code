07d376914e166e141e06b3bf215febe9854ad039
Eljay <149206706+Van1a@users.noreply.github.com>
2025-10-22 09:24:04 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "Nerium API",
-  description: "Very reliable when it comes to Game Code supported more than 150+ Games!",
+  description: "Very reliable when it comes to Game Code supported more than 500+ Games!",
 };
 
 export default function RootLayout({ children }) {
